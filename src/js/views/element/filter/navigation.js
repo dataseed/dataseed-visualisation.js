95d09bc09305a907fa1676a4c55bc4de51a5ff1b
Cavan Law <cavan.law@atchai.com>
2014-07-07 19:17:57 +0100

    Add customisable colours to filter and summary elements.
    re #582

--- src/js/views/element/filter/navigation.js
+++ src/js/views/element/filter/navigation.js
@@ -87,6 +87,16 @@ define(['backbone', 'underscore', '../filter', 'text!../../../templates/element/
 
             this.$('.dimension-filter .num-selected').css('color', this.visualisation.styles.getStyle('featureFill', this.model));
 
+            // Set the background colour
+            this.$el.css('background-color', this.visualisation.styles.getStyle('background', this.model));
+
+            // Set the element title colour
+            this.$('h2').css('color', this.visualisation.styles.getStyle('heading', this.model));
+            this.$('h3 a').css('color', this.visualisation.styles.getStyle('heading', this.model));
+
+            // Set the filter scale text
+            this.$('h3 .filter-info').css('color', this.visualisation.styles.getStyle('scaleFeature', this.model));
+
             return this;
         },
 
