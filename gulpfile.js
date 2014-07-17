c05d507d6e49cbf4479b55d5d3194c151d79fb20
Rob Hardwick <rob.hardwick@atchai.com>
2014-07-17 16:07:40 +0100

    Fix dataset featured column migration script

--- gulpfile.js
+++ gulpfile.js
@@ -8,119 +8,76 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     csso = require('gulp-csso');
 
-// Configuration
-var conf = {
-    js: {
-        test: {
-            config: 'src/js/test/karma.conf.js',
-            perfConfig: 'src/js/test/karma.perf.conf.js',
-            files: 'test/spec/**/*.js'
-        },
-        rjs: {
-            baseUrl: 'src/js',
-            mainConfigFile: 'src/js/config.js',
-            name: 'components/almond/almond',
-            include: ['app'],
-            insertRequire: ['app'],
-            almond: true,
-            inlineText: true,
-            preserveLicenseComments: false
-        },
-        charts: ['chart', 'bar', 'bubble', 'geo', 'line'],
-        outDir: 'dist/js/'
-    },
-    less: {
-        src: 'src/less/dataseed.less',
-        paths: ['src/less/'],
-        outDir: 'dist/css/'
-    }
-};
-
 // Lint JS
 gulp.task('lint', function() {
     return gulp.src([
-            conf.js.rjs.baseUrl + '/**/*.js',
-            '!' + conf.js.rjs.baseUrl + '/components/**/*.js',
-            '!' + conf.js.test.config,
-            '!' + conf.js.test.perfConfig
+            'src/js/**/*.js',
+            '!src/js/components/**/*.js',
+            '!src/js/test/karma.conf.js',
+            '!src/js/test/karma.perf.conf.js'
         ])
-        .pipe(jshint({
-            sub: true
-        }))
+        .pipe(jshint({sub: true}))
         .pipe(jshint.reporter('default'));
 });
 
 // Test JS
 gulp.task('test', function() {
-    return gulp.src(conf.js.test.files)
-        .pipe(karma({
-            configFile: conf.js.test.config,
-            action: 'run'
-        }))
-        .on('error', function(err) {
-            throw err;
-        });
-});
-
-// Test JS (watch)
-gulp.task('watch', function() {
-    return gulp.src(conf.js.test.files)
-        .pipe(karma({
-            configFile: conf.js.test.config,
-            action: 'watch',
-            logLevel: 'debug'
-        }));
+    return gulp.src('test/spec/**/*.js')
+        .pipe(karma({configFile: 'src/js/test/karma.conf.js', action: 'run'}))
+        .on('error', function(err) { throw err; });
 });
 
 // Performance Test JS
 gulp.task('test-perf', function() {
-    return gulp.src(conf.js.test.files)
-        .pipe(karma({
-            configFile: conf.js.test.perfConfig,
-            action: 'run'
-        }))
-        .on('error', function(err) {
-            throw err;
-        });
+    return gulp.src('test/spec/**/*.js')
+        .pipe(karma({configFile: 'src/js/test/karma.perf.conf.js', action: 'run'}))
+        .on('error', function(err) { throw err; });
 });
 
-// Compile JS (d3)
+// Test JS (watch)
+gulp.task('watch', function() {
+    return gulp.src('test/spec/**/*.js')
+        .pipe(karma({configFile: 'src/js/test/karma.conf.js', action: 'watch', logLevel: 'debug'}));
+});
+
+// Compile JS
+var rjs_config = {
+    baseUrl: 'src/js',
+    mainConfigFile: 'src/js/config.js',
+    name: 'components/almond/almond',
+    include: ['app'],
+    insertRequire: ['app'],
+    almond: true,
+    inlineText: true,
+    preserveLicenseComments: false
+};
+
+// D3
 gulp.task('js-d3', function() {
-    return rjs(_.extend({}, conf.js.rjs, {
-            out: 'dataseed.js'
-        }))
-        .pipe(uglify({
-            outSourceMap: false
-        }))
+    return rjs(_.extend({out: 'dataseed.js'}, rjs_config))
+        .pipe(uglify({outSourceMap: false}))
         .pipe(replace(/\s+/g, ' '))
-        .pipe(gulp.dest(conf.js.outDir));
+        .pipe(gulp.dest('dist/js/'));
 });
 
-// Compile JS (Highcharts)
+// Highcharts
 gulp.task('js-highcharts', function() {
     var basePath = 'views/element/',
-        paths = _.object(_.map(conf.js.charts, function(module) {
+        paths = _.object(_.map(['chart', 'bar', 'bubble', 'geo', 'line'], function(module) {
             return [basePath + 'd3/' + module, basePath + 'highcharts/' + module];
         }));
-    return rjs(_.extend({}, conf.js.rjs, {
-            paths: paths,
-            out: 'dataseed-highcharts.js'
-        }))
-        .pipe(uglify({
-            outSourceMap: false
-        }))
+    return rjs(_.extend({paths: paths, out: 'dataseed-highcharts.js'}, rjs_config))
+        .pipe(uglify({outSourceMap: false}))
         .pipe(replace(/\s+/g, ' '))
-        .pipe(gulp.dest(conf.js.outDir));
+        .pipe(gulp.dest('dist/js/'));
 });
 
 // Compile LESS
 gulp.task('less', function () {
-    return gulp.src(conf.less.src)
-        .pipe(less({
-            paths: conf.less.paths
-        }))
+    return gulp.src('src/less/dataseed.less')
+        .pipe(less({paths: ['src/less/']}))
         .pipe(csso())
-        .pipe(gulp.dest(conf.less.outDir));
+        .pipe(gulp.dest('dist/css/'));
 });
 
 // Default
